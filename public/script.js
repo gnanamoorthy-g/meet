02fab02dbdf97e74e37e11b8ff321c18feb43a1a
gnanamoorthy-g <ggnanamoorthy215@gmail.com>
2024-08-26 03:16:22 +0530

    func changes

--- public/script.js
+++ public/script.js
@@ -7,8 +7,10 @@ const meeting_id = path.replace("/", "");
 var socket;
 var meeting_room;
 let localStream;
-let remoteStream;
-let peerConnection;
+var exitRoomTimer
+const peerConnections = {}; // Key: connectionId, Value: RTCPeerConnection
+const remoteStreams = {};   // Key: connectionId, Value: MediaStream
+
 const SOCKET_URL = "http://192.168.29.138:8005";
 //const SOCKET_URL = "https://meet-socket-server.adaptable.app/";
 
@@ -46,7 +48,9 @@ disconnectBtn.addEventListener('click',function(event){
     meeting_id,
   });
   const { origin } = window.location;
-  window.location.replace(origin);
+  exitRoomTimer = setTimeout(()=>{
+    window.location.replace(origin);
+  },1000);
 });
 
 const initializeLocalStream = async () => {
@@ -55,98 +59,18 @@ const initializeLocalStream = async () => {
     video: currentUser.isCameraEnabled || true,
   };
   localStream = await navigator.mediaDevices.getUserMedia(constraints);
+  console.log("localStream is ::::: " ,localStream);
   const videoTracks = localStream.getVideoTracks();
   console.log(videoTracks, "videoTracks");
   video_self.srcObject = localStream;
-  localStream.onremovetrack = () => {
-    console.log("Stream ended");
-  };
-};
-
-const processIceCandidate = (sdp_func,candidate,connectionId) =>{
-  if(sdp_func && sdp_func instanceof Function) sdp_func(candidate,connectionId);
-}
-
-const handleIncomingSDP = async (sdp_func, message, from) => {
-  if (!peerConnection) {
-    peerConnection = new RTCPeerConnection(servers);
-    remoteStream = new MediaStream();
-    peerConnection.ontrack = (event) => {
-      event.streams[0].getTracks().forEach((track) => {
-        remoteStream.addTrack(track);
-      });
-      const remoteVideo = document.getElementById(from);
-      if (remoteVideo) {
-        remoteVideo.srcObject = remoteStream;
-      } else {
-        const newVideo = document.createElement("video");
-        newVideo.id = from;
-        newVideo.srcObject = remoteStream;
-        newVideo.autoplay = true;
-        document.getElementById("canvas_container").appendChild(newVideo);
-      }
+  if(localStream){
+    localStream.onremovetrack = () => {
+      console.log("Stream ended");
     };
   }
-  if (message.offer) {
-    await peerConnection.setRemoteDescription(
-      new RTCSessionDescription(message.offer)
-    );
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    sdp_func(JSON.stringify({ answer: peerConnection.localDescription }), from);
-  } else if (message.answer) {
-    await peerConnection.setRemoteDescription(
-      new RTCSessionDescription(message.answer)
-    );
-  } else if (message.iceCandidate) {
-    try {
-      await peerConnection.addIceCandidate(
-        new RTCIceCandidate(message.iceCandidate)
-      );
-    } catch (e) {
-      console.error("Error adding received ICE candidate", e);
-    }
-  }
 };
 
-const createOffer = async (sdp_func,connectionId) =>{
-  peerConnection = new RTCPeerConnection(servers);
-  remoteStream = new MediaStream();
-
-  localStream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track,localStream);
-  });
-
-  peerConnection.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack(track);
-    });
-    const remoteVideo = document.getElementById(connectionId);
-    if(remoteVideo) remoteVideo.srcObject = remoteStream;
-  };
-
-  peerConnection.onicecandidate = async (event) => {
-    if(event.candidate){
-      console.log("New ICE candidate:: ", event.candidate);
-      let candidate = JSON.stringify({iceCandidate : event.candidate});
-      processIceCandidate(sdp_func,candidate,connectionId);
-    }
-  }
-
-  peerConnection.onnegotiationneeded = async (event) => {
-    let offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-    console.log("offer :: ", offer);
-    if(sdp_func && sdp_func instanceof Function){
-      sdp_func(JSON.stringify({offer : peerConnection.localDescription}),connectionId);
-    }
-  };
-}
-
-const initMeeting = async (meeting_room,sdp_func, connectionId,) => {
-  await initializeLocalStream();
-  createOffer(sdp_func,connectionId);
-}
+initializeLocalStream();
 
 const render_meeting = (meeting) => {
   const container = document.getElementById("canvas_container");
@@ -172,11 +96,141 @@ const servers = {
   ],
 };
 
+const createNewConnection = async (sdp_func,connection_id) => {
+  let peerConnection;
+  let remoteStream;
+  peerConnection = peerConnections[connection_id];
+  remoteStream = remoteStreams[connection_id];
+  if(!peerConnection){
+    peerConnection = new RTCPeerConnection(servers);
+    remoteStream = new MediaStream();
+    console.log("**** new RTC connection created for id ::  "+connection_id + " ****");
+    peerConnections[connection_id] = peerConnection;
+    remoteStreams[connection_id] = remoteStream;
+  }
+
+  if(localStream){
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track,localStream);
+    });
+  }
+  
+  peerConnection.onicecandidate = async (event) => {
+    console.log("New ICE candidate:: ", event.candidate);
+    if(event.candidate){
+      console.log(" *** gathering candidate finished ***");
+      sdp_func('on_new_ice_candidate',event.candidate,connection_id);
+    }
+  }
+
+  peerConnection.ontrack = (event) => {
+    console.log("********onTrack****",event);
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+    const remoteVideo = document.getElementById(connection_id);
+    if(remoteVideo) remoteVideo.srcObject = remoteStream;
+  };
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  sdp_func('send_offer',offer,connection_id);
+}
+
+const acceptAnswerForOffer = async (answer,sdp_func,connection_id) => {
+  console.log(" **** sdp answer received ::",answer);
+  let peerConnection;
+  let remoteStream;
+  peerConnection = peerConnections[connection_id];
+  remoteStream = remoteStreams[connection_id];
+
+  if(!peerConnection){
+    peerConnection = new RTCPeerConnection(servers);
+    remoteStream = new MediaStream();
+    console.log("**** new RTC connection created for id ::  "+connection_id + " ****");
+    peerConnections[connection_id] = peerConnection;
+    remoteStreams[connection_id] = remoteStream;
+  }
+
+  await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+
+  peerConnection.ontrack = (event) => {
+    console.log("********onTrack****",event);
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+    const remoteVideo = document.getElementById(connection_id);
+    if(remoteVideo) remoteVideo.srcObject = remoteStream;
+  };
+}
+
+const createAnswerForOffer = async (offer,sdp_func,connection_id) =>{
+  console.log(" **** offer received ::",offer);
+  let peerConnection;
+  let remoteStream;
+  peerConnection = peerConnections[connection_id];
+  remoteStream = remoteStreams[connection_id];
+  if(!peerConnection){
+    peerConnection = new RTCPeerConnection(servers);
+    remoteStream = new MediaStream();
+    console.log("**** new RTC connection created for id ::  "+connection_id + " ****");
+    peerConnections[connection_id] = peerConnection;
+    remoteStreams[connection_id] = remoteStream;
+  }
+
+  if(localStream){
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track,localStream);
+    });
+  }
+
+  await peerConnection.setRemoteDescription( new RTCSessionDescription(offer));
+
+  peerConnection.onicecandidate = async (event) => {
+    console.log("New ICE candidate:: ", event.candidate);
+    if(event.candidate){
+      console.log(" *** gathering candidate finished ***");
+      sdp_func('on_new_ice_candidate',event.candidate,connection_id);
+    }
+  };
+
+  peerConnection.ontrack = (event) => {
+    console.log("********onTrack****",event);
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+    const remoteVideo = document.getElementById(connection_id);
+    if (remoteVideo) remoteVideo.srcObject = remoteStream;
+  };
+
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+  console.log("answer :: ", answer);
+  sdp_func("send_answer", answer, connection_id);
+}
+
+const addIceCandidate = (candidate, connection_id) => {
+  console.log(" **** new ice candidate received ::",candidate);
+  let peerConnection;
+  let remoteStream;
+  peerConnection = peerConnections[connection_id];
+  remoteStream = remoteStreams[connection_id];
+
+  if(!peerConnection){
+    peerConnection = new RTCPeerConnection(servers);
+    remoteStream = new MediaStream();
+    console.log("**** new RTC connection created for id ::  "+connection_id + " ****");
+    peerConnections[connection_id] = peerConnection;
+    remoteStreams[connection_id] = remoteStream;
+  }
+  peerConnection.addIceCandidate( new RTCIceCandidate(candidate));
+}
+
 const create_signaling_server = (user, meeting_room) => {
   socket = io(SOCKET_URL, { autoConnect: true });
 
-  var SDP_function = (data, connection_id) =>{
-    socket.emit("SDP_PROCESS",{
+  var SDP_function = (event,data, connection_id) =>{
+    socket.emit(event,{
       message : data,
       meeting_id : meeting_id,
       to : connection_id
@@ -193,7 +247,7 @@ const create_signaling_server = (user, meeting_room) => {
       let { room } = data;
       meeting_room = room;
       console.log(meeting_room,"room");
-      initMeeting(meeting_room,SDP_function,socket.id);
+
       if (socket.connected) {
         socket.emit("user_joined_meeting_room", {
           user,
@@ -216,28 +270,43 @@ const create_signaling_server = (user, meeting_room) => {
     render_meeting(meeting_room);
   });
 
-  socket.on("notify_participants", (data) => {
+  socket.on("notify_participants_about_joined_user", (data) => {
     let { meeting, new_connection_id, joined_user} = data;
     console.log(" *** new user :: " + joined_user.firstName + "  joined");
-    console.log(data, "notification");
     meeting_room = meeting;
-    createOffer(SDP_function,new_connection_id);
     render_meeting(meeting_room);
-
+    createNewConnection(SDP_function,new_connection_id);
   });
-  socket.on("SDP_PROCESS", async (data) =>{
-    let { message , from} = data;
-    message = JSON.parse(message);
-    if(message.offer){
 
-    }
-    console.log(data,"On server event --");
-    await handleIncomingSDP(SDP_function,message, from);
-  })
+  socket.on("notify_participants_about_exited_user", (data) => {
+    let { meeting, exited_connection_id, exited_user} = data;
+    console.log(" *** user :: " + exited_user.firstName + "  left");
+    meeting_room = meeting;
+    render_meeting(meeting_room);
+  });
+
+  socket.on("receive_offer",(data) => {
+    console.log(" *** offer received :: ",data);
+    let { message , from} = data;
+    createAnswerForOffer(message,SDP_function,from);
+  });
+
+  socket.on("receive_answer",(data) => {
+    console.log(" *** answer received :: ",data);
+    let { message , from} = data;
+    acceptAnswerForOffer(message,SDP_function,from);
+  });
+
+  socket.on("receive_ice_candidate", (data) => {
+    console.log(" *** ice candidate received :: ",data);
+    let { candidate , from} = data;
+    addIceCandidate(candidate, from);
+  });
 };
 
 create_signaling_server(currentUser, meeting_room);
 
 window.addEventListener("beforeunload", (event) => {
   socket.close();
+  clearTimeout(exitRoomTimer);
 });
